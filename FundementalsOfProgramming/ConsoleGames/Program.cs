b1025d7b2196c9e1d0a7c1bd4c0dd615dd04e8c4
Gabriella Emerson <2016045@folkestonegirls.kent.sch.uk>
2021-11-02 15:30:12 +0000

    Array, subroutine.

--- FundementalsOfProgramming/ConsoleGames/Program.cs
+++ FundementalsOfProgramming/ConsoleGames/Program.cs
@@ -6,7 +6,26 @@ namespace ConsoleGames
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to noughts and crosses!");
+            string[,] grid = new string[3, 3]
+                {
+                    {" ", " ", " "},
+                    {" ", " ", " "},
+                    {" ", " ", " "}
+                };
+
+            int gridLength = grid.Length;
+        }
+
+        static void checkGrid(int gridSize)
+        {
+            for (int i = 0; i < (gridSize - 1); i++)
+            {
+                if (true) //Check to see if the grid has space
+                {
+
+                }
+            }
         }
     }
 }
