cfafda771b5cd2e7226b263274199a7e8dc60df1
Gabriella Emerson <2016045@folkestonegirls.kent.sch.uk>
2021-11-04 10:15:17 +0000

    Check there is space to play, started Player Turn

--- FundementalsOfProgramming/ConsoleGames/Program.cs
+++ FundementalsOfProgramming/ConsoleGames/Program.cs
@@ -14,18 +14,54 @@ namespace ConsoleGames
                     {" ", " ", " "}
                 };
 
-            int gridLength = grid.Length;
-        }
-
-        static void checkGrid(int gridSize)
-        {
-            for (int i = 0; i < (gridSize - 1); i++)
+            do
             {
-                if (true) //Check to see if the grid has space
-                {
+                Console.WriteLine("it works?");
+                CheckGridSpace(grid);
+            } while (CheckGridSpace(grid) == true);
+            
 
-                }
-            }
+
         }
+
+        static bool CheckGridSpace(string[,] grid)
+        {
+            bool space = false;
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    if (grid[i,j] == " ") //Check to see if the grid has space
+                    {
+                        space = true;
+                
+                    }
+                }
+                
+            }
+            return space;
+        }
+
+        static void PlayerTurn(string[,] grid)
+        {
+            Console.WriteLine("Player 1 is O and Player 2 is X");
+            for (int i = 1; i < 2; i++)
+            {
+                Console.WriteLine($"Player {i}'s Turn");
+                Console.WriteLine("Please enter the row you'd like to play in (1 - 3)");
+                int Row = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter the column you'd like to play in (1 - 3)");
+                int Column = Int32.Parse(Console.ReadLine());
+                
+            }
+            
+        }
+
+        static void TurnCheck()
+        {
+
+        }
+
+       
     }
 }
