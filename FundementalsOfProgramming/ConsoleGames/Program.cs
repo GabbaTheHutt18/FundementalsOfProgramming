9f3020c8944f7b2c9a68c44c5c7402f7feb7bb47
Gabriella Emerson <2016045@folkestonegirls.kent.sch.uk>
2021-11-04 11:06:42 +0000

    Finished PlayerChoice, get row and column, check turn, Output grid function.

--- FundementalsOfProgramming/ConsoleGames/Program.cs
+++ FundementalsOfProgramming/ConsoleGames/Program.cs
@@ -9,17 +9,20 @@ namespace ConsoleGames
             Console.WriteLine("Welcome to noughts and crosses!");
             string[,] grid = new string[3, 3]
                 {
-                    {" ", " ", " "},
-                    {" ", " ", " "},
-                    {" ", " ", " "}
+                    {"-", "-", "-"},
+                    {"-", "-", "-"},
+                    {"-", "-", "-"}
                 };
 
+            OutputGrid(grid);
             do
             {
-                Console.WriteLine("it works?");
+
+                grid = PlayerChoice(grid);
+                OutputGrid(grid);
                 CheckGridSpace(grid);
             } while (CheckGridSpace(grid) == true);
-            
+
 
 
         }
@@ -27,41 +30,95 @@ namespace ConsoleGames
         static bool CheckGridSpace(string[,] grid)
         {
             bool space = false;
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < 2; j++)
+                for (int j = 0; j < 3; j++)
                 {
-                    if (grid[i,j] == " ") //Check to see if the grid has space
+                    if (grid[i, j] == "-") //Check to see if the grid has space
                     {
                         space = true;
-                
+
                     }
                 }
-                
+
             }
             return space;
         }
 
-        static void PlayerTurn(string[,] grid)
+        static string[,] PlayerChoice(string[,] grid)
         {
+            string[] playerSymbols = new string[] { "X", "O" };
             Console.WriteLine("Player 1 is O and Player 2 is X");
-            for (int i = 1; i < 2; i++)
+            for (int i = 0; i <= 1; i++)
             {
-                Console.WriteLine($"Player {i}'s Turn");
-                Console.WriteLine("Please enter the row you'd like to play in (1 - 3)");
-                int Row = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Please enter the column you'd like to play in (1 - 3)");
-                int Column = Int32.Parse(Console.ReadLine());
-                
+                Console.WriteLine($"Player {i + 1}'s Turn");
+                int row = GetRow();
+                int column = GetColumn();
+                bool validChoice = TurnCheck(grid, row, column);
+                while (validChoice == true)
+                {
+                    row = GetRow();
+                    column = GetColumn();
+                    validChoice = TurnCheck(grid, row, column);
+
+                }
+                grid[row, column] = playerSymbols[i];
+
             }
-            
+            return grid;
+
         }
 
-        static void TurnCheck()
+        static int GetRow()
+        {
+            Console.WriteLine("Please enter the row you'd like to play in (1 - 3)");
+            int row = (Int32.Parse(Console.ReadLine()) - 1);
+            return row;
+        }
+
+        static int GetColumn()
+        {
+            Console.WriteLine("Please enter the column you'd like to play in (1 - 3)");
+            int column = (Int32.Parse(Console.ReadLine()) - 1);
+            return column;
+        }
+
+
+        static bool TurnCheck(string[,] grid, int row, int column)
         {
 
+            if (grid[row, column] != "-")
+            {
+                Console.WriteLine("Invalid Choice Please try again :)");
+                return true;
+            }
+            return false;
         }
 
-       
+        static void OutputGrid(string[,] grid)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(grid[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static bool CheckWin(string [,] grid)
+        {
+            bool win = false;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                
+                }
+
+                return win;
+        }
+        
     }
 }
